7280f634b9b58c55cb32cc5edc3f317605c8ea99
Dnyanesh <dnyanesh.kokate75@gmail.com>
2023-01-17 23:06:23 -0800

    add pset solution  file pset-2.py

---
+++ string_psets/PSET-2/pset-2.py
@@ -0,0 +1,6 @@
+Welcome your Friend with name
+print(Please enter your name) # inform the user in advance
+name = input() # get name of user
+print(Welcome, name)
+print(Please enter your name) inform the user in advance
+name = input() # get name of user
