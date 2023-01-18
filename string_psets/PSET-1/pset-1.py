497e86f489b93107c437c7dfebbcfa56f931536d
Dnyanesh <dnyanesh.kokate75@gmail.com>
2023-01-17 22:45:21 -0800

    add file pset-1.py and solution

---
+++ string_psets/PSET-1/pset-1.py
@@ -0,0 +1,7 @@
+Find the USN of students and print
+name = input(Please enter your name) 
+USN = input(please enter your USN)
+# check the substring GL is present
+print(USN.find(GL))
+# print(USN.index(GL))
+print(name)
